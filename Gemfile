3abed679cb1d42fa195ac399034e8f71e8cbd1d0
ogom <ogom@hotmail.co.jp>
2014-02-27 23:44:14 +0900

    Added rspec

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in nsume.gemspec
 gemspec
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/nsume/version_spec.rb
@@ -0,0 +1,7 @@
+describe Nsume do
+  describe "VERSION" do
+    it "Nsume::VERSION" do
+      expect(Nsume::VERSION).to eq(Nsume::VERSION)
+    end
+  end
+end
