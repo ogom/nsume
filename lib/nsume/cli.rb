34ef1a6fb87c85fa4092b19f9b8f2e8f9412478e
ogom <ogom@hotmail.co.jp>
2014-03-08 23:55:37 +0900

    Added faraday

--- lib/nsume/cli.rb
+++ lib/nsume/cli.rb
@@ -1,4 +1,5 @@
 require 'erb'
+require 'faraday'
 
 module Nsume
   class Cli < Thor
@@ -14,6 +15,25 @@ module Nsume
       src = ERB.new(File.read(File.expand_path('_config.yml.erb', path))).result
       File.write(File.expand_path('_config.yml', path), src)
       FileUtils.rm File.expand_path('_config.yml.erb', path)
+
+      src = Faraday.get("https://code.jquery.com/jquery-1.10.2.min.js").body
+      File.write(File.expand_path('assets/jquery/jquery-1.10.2.min.js', path), src)
+
+      FileUtils.mkdir_p(File.expand_path('assets/bootstrap/dist/js', path))
+      src = Faraday.get("https://raw.github.com/twbs/bootstrap/v3.1.1/dist/js/bootstrap.min.js").body
+      File.write(File.expand_path('assets/bootstrap/dist/js/bootstrap.min.js', path), src)
+
+      FileUtils.mkdir_p(File.expand_path('assets/bootswatch/css', path))
+      src = Faraday.get("https://raw.github.com/thomaspark/bootswatch/v3.1.1/assets/css/bootswatch.min.css").body
+      File.write(File.expand_path('assets/bootswatch/css/bootswatch.min.css', path), src)
+
+      FileUtils.mkdir_p(File.expand_path('assets/bootswatch/js', path))
+      src = Faraday.get("https://raw.github.com/thomaspark/bootswatch/v3.1.1/assets/js/bootswatch.js").body
+      File.write(File.expand_path('assets/bootswatch/js/bootswatch.js', path), src)
+
+      FileUtils.mkdir_p(File.expand_path('assets/themes/flatly', path))
+      src = Faraday.get("https://raw.github.com/thomaspark/bootswatch/v3.1.1/flatly/bootstrap.min.css").body
+      File.write(File.expand_path('assets/themes/flatly/bootstrap.min.css', path), src)
     end
 
     desc 'version', 'Print the version and exit.'
