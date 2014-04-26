3f9b39fe274db1f23764bc6a1065bc403125e264
ogom <ogom@hotmail.co.jp>
2014-04-26 22:44:26 +0900

    Rename github to githubusercontent

--- lib/nsume/default.rb
+++ lib/nsume/default.rb
@@ -3,11 +3,11 @@ module Nsume
     JQUERY_ENDPOINT = 'https://code.jquery.com/jquery-1.10.2.min.js'.freeze
     JQUERY_FILE = 'jquery.min.js'.freeze
 
-    BOOTSTRAP_ENDPOINT = 'https://raw.github.com/twbs/bootstrap/v3.1.1'.freeze
+    BOOTSTRAP_ENDPOINT = 'https://raw.githubusercontent.com/twbs/bootstrap/v3.1.1'.freeze
     BOOTSTRAP_CSS_FILE = 'bootstrap.min.css'.freeze
     BOOTSTRAP_JS_FILE = 'bootstrap.min.js'.freeze
 
-    BOOTSWATCH_ENDPOINT = 'https://raw.github.com/thomaspark/bootswatch/v3.1.1'.freeze
+    BOOTSWATCH_ENDPOINT = 'https://raw.githubusercontent.com/thomaspark/bootswatch/v3.1.1'.freeze
     BOOTSWATCH_CSS_FILE = 'bootswatch.min.css'.freeze
     BOOTSWATCH_JS_FILE = 'bootswatch.js'.freeze
     BOOTSWATCH_THEME = 'flatly'.freeze
